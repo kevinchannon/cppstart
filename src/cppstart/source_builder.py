5e2a58aab64a85ef53c9aa5ba874ba812667f176
Kevin Channon <kevinchannon@gmail.com>
2023-04-24 07:46:33 +0100

    Add AppSourceBuilder

--- src/cppstart/source_builder.py
+++ src/cppstart/source_builder.py
@@ -5,14 +5,23 @@ from typing import Dict
 from pathlib import Path
 
 
-class SourceBuilder:
+class __SourceBuilder:
     _INCLUDE = """#include <cstdint>\n"""
     _EXAMPLES_MAIN = "#include <proj_name/proj_name.hpp>\n\nauto main() -> int {\n    return 0;\n}\n"
 
     def __init__(self, output_root: Path):
         self._output_root = output_root
+        self._proj_name = self._output_root.name
 
     def get_content(self) -> Dict[Path, str]:
-        proj_name = self._output_root.name
-        return {self._output_root / "include" / proj_name / f"{proj_name}.hpp": self._INCLUDE,
+        return {self._output_root / "include" / self._proj_name / f"{self._proj_name}.hpp": self._INCLUDE,
                 self._output_root / "examples" / "main.cpp": self._EXAMPLES_MAIN}
+
+
+class AppSourceBuilder(__SourceBuilder):
+    _PROJ_SRC = "#include <proj_name/proj_name.hpp>\n"
+
+    def get_content(self):
+        base_content = super().get_content()
+        return {**base_content,
+                self._output_root / "src" / self._proj_name / f"{self._proj_name}.cpp": self._PROJ_SRC}
--- tests/test_source_builder.py
+++ tests/test_source_builder.py
@@ -2,20 +2,25 @@ import unittest
 import sys
 from pathlib import Path
 
-from src.cppstart.source_builder import SourceBuilder
+from src.cppstart.source_builder import AppSourceBuilder
 
 
 class SourceBuilderTests(unittest.TestCase):
     def test_adds_include_file(self):
-        contents = SourceBuilder(Path("/foo")).get_content()
+        contents = AppSourceBuilder(Path("/foo")).get_content()
         self.assertTrue(Path("/foo/include/foo/foo.hpp") in contents)
         self.assertEqual(contents[Path("/foo/include/foo/foo.hpp")], "#include <cstdint>\n")
 
     def test_adds_example_files(self):
-        contents = SourceBuilder(Path("/foo")).get_content()
+        contents = AppSourceBuilder(Path("/foo")).get_content()
         self.assertTrue(Path("/foo/examples/main.cpp") in contents)
         self.assertEqual(contents[Path("/foo/examples/main.cpp")],
                          "#include <proj_name/proj_name.hpp>\n\nauto main() -> int {\n    return 0;\n}\n")
+
+    def test_adds_source_files(self):
+        contents = AppSourceBuilder(Path("/foo")).get_content()
+        self.assertTrue(Path("/foo/src/foo/foo.cpp") in contents)
+        self.assertEqual(contents[Path("/foo/src/foo/foo.cpp")], "#include <proj_name/proj_name.hpp>\n")
 
 
 if __name__ == '__main__':
