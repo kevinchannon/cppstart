34c268c83618fff2bdb1f3e59d323d8cf066b8aa
Kevin Channon <kevinchannon@gmail.com>
2023-04-25 07:08:22 +0100

    Add source_builder.make_source_builder

--- src/cppstart/source_builder.py
+++ src/cppstart/source_builder.py
@@ -3,6 +3,7 @@ from shutil import copytree
 import os
 from typing import Dict
 from pathlib import Path
+from enum import Enum
 
 
 class __SourceBuilder:
@@ -36,3 +37,15 @@ class LibSourceBuilder(__SourceBuilder):
         base_content = super().get_content()
         return {**base_content,
                 self._output_root / "src" / self._proj_name / f"{self._proj_name}.cpp": self._PROJ_SRC}
+
+
+class ProjectType(Enum):
+    APP = 0
+    LIB = 1
+
+
+def make_source_builder(project_type: ProjectType, root_dir: Path):
+    if ProjectType.APP == project_type:
+        return AppSourceBuilder(root_dir)
+    if ProjectType.LIB == project_type:
+        return LibSourceBuilder(root_dir)
--- tests/test_source_builder.py
+++ tests/test_source_builder.py
@@ -2,7 +2,7 @@ import unittest
 from parameterized import parameterized
 from pathlib import Path
 
-from src.cppstart.source_builder import AppSourceBuilder, LibSourceBuilder
+from src.cppstart.source_builder import *
 
 
 class SourceBuilderTests(unittest.TestCase):
@@ -43,6 +43,14 @@ class SourceBuilderTests(unittest.TestCase):
     def test_lib_source_builder_does_not_add_main(self):
         contents = LibSourceBuilder(Path("/foo")).get_content()
         self.assertFalse(Path("/foo/src/main.cpp") in contents)
+
+    def test_source_builder_factory_returns_an_app_builder(self):
+        builder = make_source_builder(ProjectType.APP, Path("/foo"))
+        self.assertIsInstance(builder, AppSourceBuilder)
+
+    def test_source_builder_factory_returns_a_lib_builder(self):
+        builder = make_source_builder(ProjectType.LIB, Path("/foo"))
+        self.assertIsInstance(builder, LibSourceBuilder)
 
 
 if __name__ == '__main__':
