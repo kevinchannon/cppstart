adcfa966cd30ebab0ce7a678f949aa176c7975f7
Kevin Channon <kevinchannon@gmail.com>
2023-04-26 20:03:14 +0100

    Argument parser parses target project type

--- src/cppstart/cppstart.py
+++ src/cppstart/cppstart.py
@@ -6,7 +6,9 @@ from source_builder import *
 def get_command_line_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("proj_name", help="name of the project")
-    parser.add_argument("-d", "--output_directory", default=".", help="output directory")
+    parser.add_argument("-d", "--output-directory", default=".", help="output directory")
+    parser.add_argument("-A", "--app", action="store_true", dest="is_app", help="create an application project")
+    parser.add_argument("-L", "--lib", action="store_true", dest="is_lib", help="create a library project")
 
     return parser
 
--- tests/test_cppstart.py
+++ tests/test_cppstart.py
@@ -9,9 +9,22 @@ class ArgParserTests(unittest.TestCase):
 
     def test_output_directory_is_set_when_present(self):
         self.assertEqual("bar", get_command_line_parser().parse_args(["foo", "-d", "bar"]).output_directory)
+        self.assertEqual("bar",
+                         get_command_line_parser().parse_args(["foo", "--output-directory", "bar"]).output_directory)
 
     def test_output_directory_is_set_to_dot_when_absent(self):
         self.assertEqual(".", get_command_line_parser().parse_args(["foo"]).output_directory)
+
+    def test_project_type_is_set_correctly(self):
+        self.assertTrue(get_command_line_parser().parse_args(["foo", "--lib"]).is_lib)
+        self.assertFalse(get_command_line_parser().parse_args(["foo", "--lib"]).is_app)
+        self.assertTrue(get_command_line_parser().parse_args(["foo", "-L"]).is_lib)
+        self.assertFalse(get_command_line_parser().parse_args(["foo", "-L"]).is_app)
+
+        self.assertFalse(get_command_line_parser().parse_args(["foo", "--app"]).is_lib)
+        self.assertTrue(get_command_line_parser().parse_args(["foo", "--app"]).is_app)
+        self.assertFalse(get_command_line_parser().parse_args(["foo", "-A"]).is_lib)
+        self.assertTrue(get_command_line_parser().parse_args(["foo", "-A"]).is_app)
 
 
 if __name__ == '__main__':
