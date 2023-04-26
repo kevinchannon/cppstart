4c9879a69279d8561248d1ab342ad09cc7e68ed6
Kevin Channon <kevinchannon@gmail.com>
2023-04-26 19:58:10 +0100

    Argument parser parses output directory path

--- src/cppstart/cppstart.py
+++ src/cppstart/cppstart.py
@@ -6,6 +6,7 @@ from source_builder import *
 def get_command_line_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("proj_name", help="name of the project")
+    parser.add_argument("-d", "--output_directory", default=".", help="output directory")
 
     return parser
 
--- tests/test_cppstart.py
+++ tests/test_cppstart.py
@@ -7,6 +7,12 @@ class ArgParserTests(unittest.TestCase):
     def test_first_positional_argument_is_project_name(self):
         self.assertEqual("foo", get_command_line_parser().parse_args(["foo"]).proj_name)
 
+    def test_output_directory_is_set_when_present(self):
+        self.assertEqual("bar", get_command_line_parser().parse_args(["foo", "-d", "bar"]).output_directory)
+
+    def test_output_directory_is_set_to_dot_when_absent(self):
+        self.assertEqual(".", get_command_line_parser().parse_args(["foo"]).output_directory)
+
 
 if __name__ == '__main__':
     unittest.main()
