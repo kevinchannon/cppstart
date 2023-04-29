66fb8483be40365b459c411e3e5296e9c169a688
Kevin Channon <kevinchannon@gmail.com>
2023-04-29 07:47:08 +0100

    Add default license to license generator factory

--- src/cppstart/license_generator.py
+++ src/cppstart/license_generator.py
@@ -2,8 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class LicenseGeneratorFactory:
-    def __init__(self, licences: dict[str, str]):
+    def __init__(self, licences: dict[str, str], default: str):
         self._licenses = licences
+        self._default = default
+
+    @property
+    def default_license(self):
+        return self._default
 
     def available_licenses(self):
         return [k for k in self._licenses]
--- tests/test_license_generator.py
+++ tests/test_license_generator.py
@@ -5,7 +5,12 @@ from src.cppstart.license_generator import *
 
 class LicenseGeneratorFactoryTests(unittest.TestCase):
     def test_lists_available_licenses(self):
-        self.assertEqual(["MIT", "LGPL3"], LicenseGeneratorFactory({"MIT": "MIT text", "LGPL3": "LGPL3 text"}).available_licenses())
+        self.assertEqual(["MIT", "LGPL3"],
+                         LicenseGeneratorFactory({"MIT": "MIT text", "LGPL3": "LGPL3 text"}, "MIT").available_licenses())
+
+    def test_default_id_is_correct(self):
+        self.assertEqual("MIT",
+                         LicenseGeneratorFactory(licences={"MIT": "MIT text", "LGPL3": "LGPL3 text"}, default="MIT").default_license)
 
 
 if __name__ == '__main__':
