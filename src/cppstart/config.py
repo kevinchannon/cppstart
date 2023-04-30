b240383614f41acf136eaca592adab1cb1940926
Kevin Channon <kevinchannon@gmail.com>
2023-04-30 07:26:59 +0100

    Config stores the item types in the config

--- src/cppstart/config.py
+++ src/cppstart/config.py
@@ -14,10 +14,25 @@ class Config:
         self._parser.read_string(config_str)
 
     def get(self, section: str, item: str):
-        return self._parser.get(section, item)
+        getters = {"str": self._parser.get,
+                   "int": self._parser.getint,
+                   "float": self._parser.getfloat,
+                   "bool": self._parser.getboolean}
+
+        for t in getters:
+            typed_item = f"{item}@{t}"
+            if self._parser.has_option(section, typed_item):
+                return getters[t](section, typed_item)
 
     def set(self, section: str, item: str, value):
         if not self._parser.has_section(section):
             self._parser.add_section(section)
 
-        self._parser.set(section, item, str(value))
+        if isinstance(value, bool):
+            self._parser.set(section, f"{item}@bool", str(value))
+        elif isinstance(value, int):
+            self._parser.set(section, f"{item}@int", str(value))
+        elif isinstance(value, float):
+            self._parser.set(section, f"{item}@float", str(value))
+        else:
+            self._parser.set(section, f"{item}@str", str(value))
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
+from parameterized import parameterized
 
 from src.cppstart.config import *
 
@@ -8,7 +9,7 @@ class ConfigTests(unittest.TestCase):
     _file_reader = FileReader()
 
     def setUp(self) -> None:
-        self._file_reader.read = MagicMock(return_value="[section1]\nsetting1 = Person 123\n")
+        self._file_reader.read = MagicMock(return_value="[section1]\nsetting1@str = Person 123\n")
 
     def test_read_reads_the_config_file(self):
         config = Config(self._file_reader)
@@ -21,7 +22,34 @@ class ConfigTests(unittest.TestCase):
         config = Config(self._file_reader)
         config.set("section1", "setting1", 1000)
 
-        self.assertEqual("1000", config.get("section1", "setting1"))
+        self.assertEqual(1000, config.get("section1", "setting1"))
+
+    @parameterized.expand([
+        ("str", "Person 123"),
+        ("int", 12345),
+        ("float", 3.14152),
+        ("bool", False),
+        ("bool", True)
+    ])
+    def test_get_returns_value_with_type(self, item_type, value):
+        self._file_reader.read = MagicMock(return_value=f"[section1]\nsetting1@{item_type} = {value}\n")
+        config = Config(self._file_reader)
+        config.read(Path("the/config/path.ini"))
+
+        self.assertEqual(value, config.get("section1", "setting1"))
+
+    @parameterized.expand([
+        ("str", "Person 123"),
+        ("int", 12345),
+        ("float", 3.14152),
+        ("bool", False),
+        ("bool", True)
+    ])
+    def test_set_adds_value_with_type(self, _, value):
+        config = Config(self._file_reader)
+        config.set("section1", "setting1", value)
+
+        self.assertEqual(value, config.get("section1", "setting1"))
 
 
 if __name__ == '__main__':
